a940ee521d75016d2e3320748995e2292e97ba9e
Rishav Kumar Mishra <rishavmishra2000@gmail.com>
2022-08-30 01:16:37 +0530

    Static Variable and method Concept with Example

--- StaticVariableAndMethodsDemo.java
+++ StaticVariableAndMethodsDemo.java
@@ -1,24 +1,38 @@
 // Java Static Variable and method Demo
 
 class StaticDemo{
-	public static String pname = "";
+	public static String name = "";
 	
-	public static void setName(String name){
-	    pname = name;
+	public StaticDemo(String name){
+	    StaticDemo.name = name;
+	}
+	
+	public static void printName(){
+		System.out.println("Name= " + name);
 	}
 }
 
 class Main{
 	public static void main(String []args){
 		// Referencing the static variable using the class name 
-		StaticDemo.pname = "Rishav";
-		System.out.println(StaticDemo.pname);
+		StaticDemo demo = new StaticDemo("Rishav"); // create object demo and initize with Rishav
+		StaticDemo demo1 = new StaticDemo("Vaibhav"); // create object demo and initize with Vaibhav
+		demo.printName(); // Need to print Rishav
+ 		demo1.printName(); // Need to print Vaibhav
+		
+		/*
+			Outout:-
+				Name= Vaibhav
+				Name= Vaibhav
+		*/
 		
 		// Referencing the static variable from the class object
-		StaticDemo demo = new StaticDemo();
-		demo.setName("Vaibhav");
-		System.out.println(demo.pname);
-		
+		StaticDemo.printName();
+		printHello();		
 		// Note: we cannot access static variable/method from non-static variable/method.
+	}
+	
+	public static void printHello(){
+			System.out.println("Hellow" + StaticDemo.name);
 	}
 }
